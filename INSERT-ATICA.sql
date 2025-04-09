deeed209c2c1c04dc8831bffce150a554a9d6d17
jessuss00 <jesusmadro23@gmail.com>
2025-04-09 13:05:20 +0200

    dfdd

--- INSERT-ATICA.sql
+++ INSERT-ATICA.sql
@@ -300,88 +300,184 @@ select * from practicas;
 -- Alumno 1
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Buen desarrollo de interfaces', 8.5, TRUE, TRUE, 1, 1, 1),  -- Juan Pérez en Desarrollo Frontend
-('Excelente API RESTful', 9.0, TRUE, TRUE, 2, 1, 1),         -- Juan Pérez en Desarrollo Backend
-('Buena operación de la máquina', 7.5, TRUE, TRUE, 3, 1, 1),  -- Juan Pérez en Mantenimiento de Máquinas
-('Buena operación de la máquina', 7.5, TRUE, TRUE, 4, 1, 1),  -- Juan Pérez en Mantenimiento de Máquinas
-('Correcta programación del robot', 8.0, TRUE, TRUE, 5, 1, 1); -- Juan Pérez en Programación de Robots
+('Buen desarrollo de interfaces', 8.5, TRUE, TRUE, 1, 1, 1),  
+('Excelente API RESTful', 9.0, TRUE, TRUE, 2, 1, 1),         
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 3, 1, 1),  
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 4, 1, 1),  
+('Correcta programación del robot', 8.0, TRUE, TRUE, 5, 1, 1); 
 
 -- Alumno 2
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Buen desarrollo de interfaces', 7.8, TRUE, TRUE, 1, 2, 1),  -- Ana García en Desarrollo Frontend
-('Buena API RESTful', 8.0, TRUE, TRUE, 2, 2, 1),             -- Ana García en Desarrollo Backend
-('Buena operación de la máquina', 7.0, TRUE, TRUE, 3, 2, 1),  -- Ana García en Mantenimiento de Máquinas
-('Programación eficiente del robot', 8.3, TRUE, TRUE, 4, 2, 1); -- Ana García en Programación de Robots
+('Buen desarrollo de interfaces', 7.8, TRUE, TRUE, 6, 2, 2),  
+('Buena API RESTful', 8.0, TRUE, TRUE, 7, 2, 2),             
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 8, 2, 2), 
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 9, 2, 2),  
+('Programación eficiente del robot', 8.3, TRUE, TRUE, 10, 2, 2); 
 
 -- Alumno 3
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz simple pero funcional', 7.0, TRUE, TRUE, 1, 3, 1),  -- Mario Ruiz en Desarrollo Frontend
-('Buena API RESTful', 8.0, TRUE, TRUE, 2, 3, 1),             -- Mario Ruiz en Desarrollo Backend
-('Problemas con la máquina', 6.0, TRUE, TRUE, 3, 3, 1),       -- Mario Ruiz en Mantenimiento de Máquinas
-('Correcta programación del robot', 7.5, TRUE, TRUE, 4, 3, 1); -- Mario Ruiz en Programación de Robots
+('Interfaz simple pero funcional', 7.0, TRUE, TRUE, 11, 3, 3), 
+('Buena API RESTful', 8.0, TRUE, TRUE, 12, 3, 1),            
+('Problemas con la máquina', 6.0, TRUE, TRUE, 13, 3, 3), 
+('Problemas con la máquina', 6.0, TRUE, TRUE, 14, 3, 3),      
+('Correcta programación del robot', 7.5, TRUE, TRUE, 15, 3, 3); 
 
 -- Alumno 4
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Excelente interfaz de usuario', 9.0, TRUE, TRUE, 1, 4, 1),  -- Laura Gómez en Desarrollo Frontend
-('Buen manejo de API RESTful', 8.5, TRUE, TRUE, 2, 4, 1),     -- Laura Gómez en Desarrollo Backend
-('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 3, 4, 1), -- Laura Gómez en Mantenimiento de Máquinas
-('Excelente programación de robots', 9.5, TRUE, TRUE, 4, 4, 1); -- Laura Gómez en Programación de Robots
+('Excelente interfaz de usuario', 9.0, TRUE, TRUE, 16, 4, 4),
+('Buen manejo de API RESTful', 8.5, TRUE, TRUE, 17, 4, 4),     
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 18, 4, 4),
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 19, 4, 4), 
+('Excelente programación de robots', 9.5, TRUE, TRUE, 20, 4, 4); 
 
 -- Alumno 5
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz muy atractiva', 8.3, TRUE, TRUE, 1, 5, 1),  -- Sergio Pérez en Desarrollo Frontend
-('API RESTful bien diseñada', 8.8, TRUE, TRUE, 2, 5, 1), -- Sergio Pérez en Desarrollo Backend
-('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 3, 5, 1), -- Sergio Pérez en Mantenimiento de Máquinas
-('Programación del robot eficiente', 8.2, TRUE, TRUE, 4, 5, 1); -- Sergio Pérez en Programación de Robots
+('Interfaz muy atractiva', 8.3, TRUE, TRUE, 21, 5, 5), 
+('API RESTful bien diseñada', 8.8, TRUE, TRUE, 22, 5, 5), 
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 23, 5, 5),
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 24, 5, 5), 
+('Programación del robot eficiente', 8.2, TRUE, TRUE, 25, 5, 5); 
 
 -- Alumno 6
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz funcional pero simple', 7.5, TRUE, TRUE, 1, 6, 1),  -- Marta López en Desarrollo Frontend
-('API RESTful funcional', 7.9, TRUE, TRUE, 2, 6, 1),           -- Marta López en Desarrollo Backend
-('Buen mantenimiento de la máquina', 8.0, TRUE, TRUE, 3, 6, 1),  -- Marta López en Mantenimiento de Máquinas
-('Buen manejo en programación de robots', 8.3, TRUE, TRUE, 4, 6, 1); -- Marta López en Programación de Robots
+('Buen desarrollo de interfaces', 8.5, TRUE, TRUE, 1, 6, 6),  
+('Excelente API RESTful', 9.0, TRUE, TRUE, 2, 6, 6),         
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 3, 6, 6),  
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 4, 6, 6),  
+('Correcta programación del robot', 8.0, TRUE, TRUE, 5, 6, 6); 
 
 -- Alumno 7
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz básica pero útil', 6.8, TRUE, TRUE, 1, 7, 1),  -- Javier Martínez en Desarrollo Frontend
-('API RESTful buena pero mejorable', 7.5, TRUE, TRUE, 2, 7, 1), -- Javier Martínez en Desarrollo Backend
-('Mantenimiento de la máquina adecuado', 7.2, TRUE, TRUE, 3, 7, 1), -- Javier Martínez en Mantenimiento de Máquinas
-('Programación del robot correcta', 7.5, TRUE, TRUE, 4, 7, 1); -- Javier Martínez en Programación de Robots
+('Buen desarrollo de interfaces', 7.8, TRUE, TRUE, 6, 7, 7),  
+('Buena API RESTful', 8.0, TRUE, TRUE, 7, 7, 7),             
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 8, 7, 7), 
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 9, 7, 7),  
+('Programación eficiente del robot', 8.3, TRUE, TRUE, 10, 7, 7); 
 
 -- Alumno 8
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Buen diseño de la interfaz', 8.1, TRUE, TRUE, 1, 8, 1),  -- Claudia Rodríguez en Desarrollo Frontend
-('API RESTful eficiente', 8.4, TRUE, TRUE, 2, 8, 1),      -- Claudia Rodríguez en Desarrollo Backend
-('Mantenimiento de la máquina sin inconvenientes', 7.5, TRUE, TRUE, 3, 8, 1), -- Claudia Rodríguez en Mantenimiento de Máquinas
-('Robots programados correctamente', 8.0, TRUE, TRUE, 4, 8, 1); -- Claudia Rodríguez en Programación de Robots
+('Interfaz simple pero funcional', 7.0, TRUE, TRUE, 11, 8, 8), 
+('Buena API RESTful', 8.0, TRUE, TRUE, 12, 8,8),            
+('Problemas con la máquina', 6.0, TRUE, TRUE, 13, 8, 8), 
+('Problemas con la máquina', 6.0, TRUE, TRUE, 14, 8, 8),      
+('Correcta programación del robot', 7.5, TRUE, TRUE, 15, 8, 8); 
 
 -- Alumno 9
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz de usuario funcional', 7.5, TRUE, TRUE, 1, 9, 1),  -- Luis Torres en Desarrollo Frontend
-('API RESTful bien implementada', 8.3, TRUE, TRUE, 2, 9, 1),  -- Luis Torres en Desarrollo Backend
-('Mantenimiento de la máquina bien ejecutado', 7.2, TRUE, TRUE, 3, 9, 1), -- Luis Torres en Mantenimiento de Máquinas
-('Excelente programación de robots', 8.7, TRUE, TRUE, 4, 9, 1); -- Luis Torres en Programación de Robots
+('Excelente interfaz de usuario', 9.0, TRUE, TRUE, 16, 9, 9),
+('Buen manejo de API RESTful', 8.5, TRUE, TRUE, 17, 9, 1),     
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 18, 9, 9),
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 19, 9, 9), 
+('Excelente programación de robots', 9.5, TRUE, TRUE, 20, 9, 9); 
 
 -- Alumno 10
 INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
 VALUES 
-('Interfaz atractiva pero mejorable', 7.9, TRUE, TRUE, 1, 10, 1), -- Pedro Ruiz en Desarrollo Frontend
-('API RESTful excelente', 9.2, TRUE, TRUE, 2, 10, 1),           -- Pedro Ruiz en Desarrollo Backend
-('Mantenimiento de la máquina bien realizado', 7.6, TRUE, TRUE, 3, 10, 1), -- Pedro Ruiz en Mantenimiento de Máquinas
-('Robots programados eficientemente', 8.5, TRUE, TRUE, 4, 10, 1); -- Pedro Ruiz en Programación de Robots
+('Interfaz muy atractiva', 8.3, TRUE, TRUE, 21, 10, 10), 
+('API RESTful bien diseñada', 8.8, TRUE, TRUE, 22, 10, 1), 
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 23, 10, 10),
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 24, 10, 10), 
+('Programación del robot eficiente', 8.2, TRUE, TRUE, 25, 10, 10); 
 
--- Continúa para los demás alumnos...
+-- Alumno 11
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Buen desarrollo de interfaces', 8.5, TRUE, TRUE, 1, 11, 11),  
+('Excelente API RESTful', 9.0, TRUE, TRUE, 2, 11, 11),         
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 3, 11, 11),  
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 4, 11, 11),  
+('Correcta programación del robot', 8.0, TRUE, TRUE, 5, 11, 11); 
 
+-- Alumno 12
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Buen desarrollo de interfaces', 7.8, TRUE, TRUE, 6, 12, 12),  
+('Buena API RESTful', 8.0, TRUE, TRUE, 7, 12, 12),             
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 8, 12, 12), 
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 9, 12, 12),  
+('Programación eficiente del robot', 8.3, TRUE, TRUE, 10, 12, 12); 
 
-select * from evaluaciones;
+-- Alumno 13
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Interfaz simple pero funcional', 7.0, TRUE, TRUE, 11, 13, 13), 
+('Buena API RESTful', 8.0, TRUE, TRUE, 12, 13, 13),            
+('Problemas con la máquina', 6.0, TRUE, TRUE, 13, 13, 13), 
+('Problemas con la máquina', 6.0, TRUE, TRUE, 14, 13, 13),      
+('Correcta programación del robot', 7.5, TRUE, TRUE, 15, 13, 13); 
+
+-- Alumno 14
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Excelente interfaz de usuario', 9.0, TRUE, TRUE, 16, 14, 14),
+('Buen manejo de API RESTful', 8.5, TRUE, TRUE, 17, 14, 14),     
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 18, 14, 14),
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 19, 14, 14), 
+('Excelente programación de robots', 9.5, TRUE, TRUE, 20, 14, 14); 
+
+-- Alumno 15
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Interfaz muy atractiva', 8.3, TRUE, TRUE, 21, 15, 15), 
+('API RESTful bien diseñada', 8.8, TRUE, TRUE, 22, 15, 1), 
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 23, 15, 15),
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 24, 15, 15), 
+('Programación del robot eficiente', 8.2, TRUE, TRUE, 25, 15, 15); 
+
+-- Alumno 16
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Buen desarrollo de interfaces', 8.5, TRUE, TRUE, 1, 16, 1),  
+('Excelente API RESTful', 9.0, TRUE, TRUE, 2, 16, 1),         
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 3, 16, 1),  
+('Buena operación de la máquina', 7.5, TRUE, TRUE, 4, 16, 1),  
+('Correcta programación del robot', 8.0, TRUE, TRUE, 5, 16, 1); 
+
+-- Alumno 17
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Buen desarrollo de interfaces', 7.8, TRUE, TRUE, 6, 17, 2),  
+('Buena API RESTful', 8.0, TRUE, TRUE, 7, 17, 2),             
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 8, 17, 2), 
+('Buena operación de la máquina', 7.0, TRUE, TRUE, 9, 17, 2),  
+('Programación eficiente del robot', 8.3, TRUE, TRUE, 10, 17, 2); 
+
+-- Alumno 18
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Interfaz simple pero funcional', 7.0, TRUE, TRUE, 11, 18, 3), 
+('Buena API RESTful', 8.0, TRUE, TRUE, 12, 18, 3),            
+('Problemas con la máquina', 6.0, TRUE, TRUE, 13, 18, 3), 
+('Problemas con la máquina', 6.0, TRUE, TRUE, 14, 18, 3),      
+('Correcta programación del robot', 7.5, TRUE, TRUE, 15, 18, 3); 
+
+-- Alumno 19
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Excelente interfaz de usuario', 9.0, TRUE, TRUE, 16, 19, 4),
+('Buen manejo de API RESTful', 8.5, TRUE, TRUE, 17, 19, 4),     
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 18, 19, 4),
+('Mantenimiento adecuado de la máquina', 7.8, TRUE, TRUE, 19, 19, 4), 
+('Excelente programación de robots', 9.5, TRUE, TRUE, 20, 19, 4); 
+
+-- Alumno 20
+INSERT INTO evaluacion (observaciones_tutor_laboral, nota, firma_tutor, firma_alumno, actividad, alumno, tutor_laboral) 
+VALUES 
+('Interfaz muy atractiva', 8.3, TRUE, TRUE, 21, 20, 5), 
+('API RESTful bien diseñada', 8.8, TRUE, TRUE, 22, 20, 5), 
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 23, 20, 5),
+('Mantenimiento de la máquina sin problemas', 7.0, TRUE, TRUE, 24, 20, 5), 
+('Programación del robot eficiente', 8.2, TRUE, TRUE, 25, 20, 5); 
+
+select * from evaluacion;
 
 
 
